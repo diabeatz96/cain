10b15df388a72aef1d086bd053e6037cd5fe2baf
Adam Kostandy <adamkostandy@gmail.com>
2024-08-17 11:17:55 -0400

    Added mundane actors sheet for basic NPCS

--- module/data/actor-mundane.mjs
+++ module/data/actor-mundane.mjs
@@ -7,13 +7,26 @@ export default class CainMundane extends CainActorBase {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
-    schema.cr = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
-    schema.xp = new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 });
-    
+   
+    schema.executionTalisman = new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0 });
+    schema.category = new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0, max: 7 });
+    schema.complication = new fields.StringField({ required: true, nullable: false, initial: "" });
+    schema.threats = new fields.StringField({ required: true, nullable: false, initial: "" });
+    schema.description = new fields.StringField({ required: true, nullable: false, initial: "" });
+
+    schema.rollData = new fields.SchemaField({
+      roll1: new fields.StringField({ initial: "" }),
+      roll2: new fields.StringField({ initial: "" }),
+      roll3: new fields.StringField({ initial: "" }),
+      roll4: new fields.StringField({ initial: "" }),
+      roll5: new fields.StringField({ initial: "" }),
+      roll6: new fields.StringField({ initial: "" }),
+    });
+
     return schema
   }
 
   prepareDerivedData() {
-    this.xp = this.cr * this.cr * 100;
+
   }
 }
