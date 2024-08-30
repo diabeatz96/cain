b7fc166ef51b673a3ee7602e71affc5bd521e200
ahandleman@wustl.edu <ahandleman@wustl.edu>
2024-08-29 21:49:57 -0500

    removed some unnecessary console logs

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -347,9 +347,7 @@ export class CainActorSheet extends ActorSheet {
     }
 
     const selectedOption = selectElement.options[selectElement.selectedIndex];
-    console.log(selectedOption);
     const description = selectedOption.getAttribute('data-description');
-    console.log(description);
     document.getElementById('abilityDescription').innerText = description;
   }
 
