de200cc315a46f120c05bf44772ecb408444c95f
louvien <42213557+louisriehl@users.noreply.github.com>
2025-11-26 20:07:30 -0800

    initial setup for domain

--- module/data/domains/item-domain.mjs
+++ module/data/domains/item-domain.mjs
@@ -5,8 +5,10 @@ export default class CainDomain extends CainItemBase {
     const fields = foundry.data.fields;
     const schema = super.defineSchema();
 
+    schema.sinSource = new fields.StringField({ required: true, nullable: false, initial: "ogre" })
     schema.domainName = new fields.StringField({ required: true, nullable: false, initial: "New Domain" });
-    schema.domainDescription = new fields.StringField({ required: true, nullable: false, initial: "Domain Description" });
+    schema.domainDescription = new fields.StringField({required: true, nullable: false, initial: "Domain Description"});
+    schema.selectsExorcist = new fields.BooleanField({required: true, nullable: false, initial: false});
 
     return schema;
   }
